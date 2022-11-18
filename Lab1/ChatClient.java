d25dce86fe2002361757687ba12f474e286035f8
danti58 <dante.nilsson28@gmail.com>
2022-11-18 10:26:55 +0100

    Stuffs

--- Lab1/ChatClient.java
+++ Lab1/ChatClient.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
 
@@ -8,14 +7,22 @@ public class ChatClient {
         int port = 8080;
         Socket socket;
         String address ="localhost"; //hej
-        OutputStreamWriter out;
+        //OutputStreamWriter out;
+        PrintWriter out;
         try {
+
+            InputStreamReader i = new InputStreamReader(System.in);
+            BufferedReader b = new BufferedReader(i);
+            /*System.out.println("Enter Course");
+            String in = b.readLine();
+            System.out.println(in);*/
             socket = new Socket(address, port);
-            out = new OutputStreamWriter(socket.getOutputStream());
+            //out = new OutputStreamWriter(socket.getOutputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
 
-            out.write("fabled");
 
-            out.close();
+            //out.write("in");
+            //out.close();
 
 
 
