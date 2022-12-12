0076dab9ce1c515dd34c17895755341b948b0e85
abaz <no.sayers.original@gmail.com>
2022-12-13 00:45:09 +0100

    reading on the subject still

--- Lab3/SSLClient.java
+++ Lab3/SSLClient.java
@@ -1,2 +1,49 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+import java.net.Socket;
+
 public class SSLClient {
+    //går över till kryptering under sessionen
+    public static void main(String[] args) throws IOException {
+        int port = 587;
+        String server = "smtp.kth.se";
+        String protocol = "STARTTLC";
+        String name = "";
+
+        //create Socket
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) factory.createSocket(server, port);
+
+        //connect
+        Writer outputStreamWriter = new OutputStreamWriter(socket.getOutputStream(),
+                "US-ASCII");
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        //Login
+        outputStreamWriter.write("a001 LOGIN"); //TODO
+        //outputStreamWriter.write(username);
+        outputStreamWriter.flush();
+
+
+
+        //To read password - Ta bort?
+        char[] password = System.console().readPassword();
+
+        Writer out = new OutputStreamWriter(socket.getOutputStream(),
+                "US-ASCII");
+        out.write("Name: "+ name +"\r\n");
+        out.write("Product-ID: 67X-89\r\n");
+        out.write("Address: 1280 Deniston Blvd, NY NY 10003\r\n");
+        out.write("Card number: 4000-1234-5678-9017\r\n");
+        out.write("Expires: 08/05\r\n");
+        out.flush();
+
+
+        //En session betsår utav:
+        //val av assymmetrisk nyckel
+        //val av symmestrisk nyckel
+        //en hash algoritm
+        //(handskakningen skiljer åt lite mellan SSL och TLS)
+    }
 }
--- Lab3/SSLServer.java
+++ Lab3/SSLServer.java
@@ -1,2 +1,50 @@
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
 public class SSLServer {
+    //börjar med krypterad session TODO make try catch
+    public static void main(String[] args) throws KeyStoreException, NoSuchAlgorithmException, IOException {
+        int port = 993;
+        String server = "webmail.kth.se";
+        String protocol = "SSL";
+        String username = "abaz";
+
+
+        //create Socket
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) factory.createSocket(server, port);
+
+        //connect
+        Writer outputStreamWriter = new OutputStreamWriter(socket.getOutputStream(),
+                "US-ASCII");
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        //To read password
+        String password = String.valueOf(System.console().readPassword());
+
+        //Login
+        outputStreamWriter.write("a001 LOGIN "+username+" " + password); //TODO
+        //outputStreamWriter.write();
+        outputStreamWriter.flush();
+
+        KeyStore ks = null;
+        ks = KeyStore.getInstance(protocol);
+        //InputStream in = null;
+        //in = new FileInputStream(new File());
+
+
+
+        //ks.load();
+
+        SSLContext ctx = SSLContext.getInstance("TLS");
+
+
+
+    }
 }
