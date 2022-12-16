f6fc7db029feb4679c2afe31e65d2f243d71c788
danti58 <dante.nilsson28@gmail.com>
2022-12-16 16:50:06 +0100

    SSLClient is compleat

--- Lab3/SSLClient.java
+++ Lab3/SSLClient.java
@@ -10,9 +10,9 @@ public class SSLClient {
     public static void main(String[] args) throws IOException {
         int port = 587;
         String server = "smtp.kth.se";
-        //String protocol = "STARTTLC";
 
 
+        //Sätter upp koppling till server
         Socket socket = new Socket(server, port);
         Writer outputStreamWriter = new OutputStreamWriter(socket.getOutputStream(),
                 "US-ASCII");
@@ -21,15 +21,8 @@ public class SSLClient {
         makeRequests("EHLO " + server , outputStreamWriter, in);
         makeRequests("STARTTLS", outputStreamWriter, in);
 
-        /*outputStreamWriter.write("EHLO " + server);
-        outputStreamWriter.write("\r\n");
-        outputStreamWriter.flush();
 
-        outputStreamWriter.write("STARTTTLS");
-        outputStreamWriter.write("\r\n");
-        outputStreamWriter.flush();*/
-
-
+        //Läser igenom anropen
         String msg = "";
         while (msg != null) {
             msg = in.readLine();
@@ -40,152 +33,75 @@ public class SSLClient {
         }
 
 
-        //create SSLSocket
+        //Skapar en säker koppling
         SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
         SSLSocket sslSocket = (SSLSocket) factory.createSocket(socket, socket.getInetAddress().getHostAddress(), port, true);
 
-        //connect
         Writer sslOutputStreamWriter = new OutputStreamWriter(sslSocket.getOutputStream());
         BufferedReader sslIn = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
 
+        //Läser in användarnamn
         System.out.println("Enter KTH username: ");
         String username = System.console().readLine();
 
 
-        //Encrypt username in Base 64
+        //Krypeterar användarnamn i Base 64
         String encryptedUsername = Base64.getEncoder().encodeToString(username.getBytes());
 
-        //To read password
+        //Läser in lösenord
         System.out.println("Write your password: ");
         String password = String.valueOf(System.console().readPassword());
 
-        //Encrypt password in Base64
+        //Krypterar lösenord i Base 64
         String encryptedPassword = Base64.getEncoder().encodeToString(password.getBytes());
 
+        //Läser in mottagare
         System.out.println("Enter receiver: ");
         String receiver = System.console().readLine();
 
+        //Initiera konversation och authentisera inloggning
         makeRequests("EHLO " + server, sslOutputStreamWriter, sslIn);
-        makeRequests("AUTH LOGIN", sslOutputStreamWriter, sslIn);
 
+        makeRequests("AUTH LOGIN", sslOutputStreamWriter, sslIn);
         makeRequests(encryptedUsername, sslOutputStreamWriter, sslIn);
         makeRequests(encryptedPassword, sslOutputStreamWriter, sslIn);
 
-        msg = "";
+
         while ((msg=sslIn.readLine()) != null) {
-            //msg = sslIn.readLine();
             System.out.println(msg);
 
-            if (msg.contains("334")) {
-                System.out.println(sslIn.readLine());
-                System.out.println(sslIn.readLine());
-                makeRequests("MAIL FROM:" + "<" + username + ">", sslOutputStreamWriter, sslIn);
+            if (msg.contains("235 2.7.0")) {
+                makeRequests("MAIL FROM:" + "<" + username + "@kth.se>", sslOutputStreamWriter, sslIn);
                 makeRequests("RCPT TO:" + "<" + receiver + ">", sslOutputStreamWriter, sslIn);
                 makeRequests("DATA", sslOutputStreamWriter, sslIn);
-                makeRequests("QUIT", sslOutputStreamWriter, sslIn);
                 writeMessage("Date: " + LocalTime.now(), sslOutputStreamWriter);
-                writeMessage("From: SMTP User " + "<" + username + ">", sslOutputStreamWriter);
+                writeMessage("From: " + "<" + username + "@kth.se>", sslOutputStreamWriter);
                 writeMessage("Subject: Testytime", sslOutputStreamWriter);
                 writeMessage("To: " + receiver, sslOutputStreamWriter);
-                //writeMessage("", sslOutputStreamWriter);
                 writeMessage("Email testing time!", sslOutputStreamWriter);
                 writeMessage(".", sslOutputStreamWriter);
                 makeRequests("QUIT", sslOutputStreamWriter, sslIn);
 
-                while ((msg=sslIn.readLine()) != null){
-                    System.out.println(msg);
-                }
             }
         }
 
-        /*
-        sslOutputStreamWriter.write("EHLO " + server);
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
 
-        sslOutputStreamWriter.write("AUTH LOGIN");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write(encryptedUsername);
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write(encryptedPassword);
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write("MAIL FROM:" + "<" + username + ">");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write("RCPT TO:" + "<" + receiver + ">");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write("DATA");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write("Date: " + LocalTime.now());
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write("From: SMTP User " + "<" + username + ">");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write("Subject: Testytime");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write("To: " + receiver);
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-         */
-
-
-
-        /*sslOutputStreamWriter.write("");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();*/
-
-        /*
-        sslOutputStreamWriter.write("Email testing time!");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write(".");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
-        sslOutputStreamWriter.write("QUIT");
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-        */
+        socket.close();
+        outputStreamWriter.close();
+        in.close();
 
         sslOutputStreamWriter.close();
         sslSocket.close();
-
-
-
-        /*
-
-        //Login
-        sslOutputStreamWriter.write("a001 LOGIN " //a001 is the tag
-                + username + " " + password);
-        sslOutputStreamWriter.write("\r\n");
-        sslOutputStreamWriter.flush();
-
+        sslIn.close();
 
         //En session betsår utav:
         //val av assymmetrisk nyckel
         //val av symmestrisk nyckel
         //en hash algoritm
         //(handskakningen skiljer åt lite mellan SSL och TLS)
-         */
-    }
 
+    }
+    
     private static void makeRequests(String message, Writer out, BufferedReader in){
         try {
             out.write(message);
@@ -193,7 +109,7 @@ public class SSLClient {
             out.flush();
             System.out.println(message);
 
-            //System.out.println(in.readLine());
+            System.out.println(in.readLine());
 
         } catch (Exception e){
             e.printStackTrace();
